72df9304cdd61b81155313fbe597a92f9f4b93a5
Mira Chen <mchen@rmn.com>
2016-01-12 23:27:52 -0600

    added game profile

---
+++ gamepoll/GPGameProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GPGameProfileViewController.swift
+//  
+//
+//  Created by Mira Chen on 1/12/16.
+//
+//
+
+import UIKit
+
+class GPGameProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismissSelf(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
